6849d212eccc18cfcb84969610085dbd9c13b904
Diak <diak@petrik>
2023-03-07 13:03:29 +0100

    Simple chart

--- src/main/java/com/example/vizsgaremek_javafx/StatController.java
+++ src/main/java/com/example/vizsgaremek_javafx/StatController.java
@@ -1,18 +1,26 @@
 package com.example.vizsgaremek_javafx;
 
+import com.google.gson.Gson;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.chart.StackedBarChart;
-import javafx.scene.control.Alert;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 
 public class StatController extends AlertController{
+
     @FXML
     private Button btnFelvetel;
     @FXML
@@ -21,6 +29,33 @@ public class StatController extends AlertController{
     private Button btnExit;
     @FXML
     private Button btnUser;
+    @FXML
+    private BarChart barChart;
+
+    private User[] users;
+
+    @FXML
+    private void initialize() throws IOException {
+        loadUsers();
+        final  String male="Férfi";
+        final  String female="Nő";
+
+        XYChart.Series series1=new XYChart.Series();
+        series1.setName("Nem");
+        series1.getData().add(new XYChart.Data("Felhasználók", users));
+
+
+        barChart.getData().addAll(series1);
+    }
+
+    private void loadUsers()throws IOException {
+        Response response=RequestHandler.get(User.USER_URL);
+        String content=response.getContent();
+        Gson converter=new Gson();
+        users=converter.fromJson(content, User[].class);
+
+
+    }
 
     @FXML
     public void clickFelvetel(ActionEvent actionEvent) {
